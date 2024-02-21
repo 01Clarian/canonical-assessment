b353f621451e4aaea08b55aaf4fa950755fafb6c
01clarian <clarionnorth@gmail.com>
2024-02-21 16:09:09 -0600

    linting resolve in context

--- src/components/context/ContextAreaOne.tsx
+++ src/components/context/ContextAreaOne.tsx
@@ -14,7 +14,7 @@ const ContextAreaOne = () => {
                 <h4 className="canonical-assessment-section-subtitle-5">
                 Outline your thoughts on the mission of Canonical. 
                   <br />  <br /> 
-                  What is it about the company's purpose and goals which is most appealing to you? What is risky or unappealing? 
+                  What is it about the company&apos;s purpose and goals which is most appealing to you? What is risky or unappealing? 
                   <br /> <br />  
                   Are there any elements of the company goals that you are unsure about?
                 </h4>
@@ -26,7 +26,7 @@ const ContextAreaOne = () => {
       <p>
         As a passionate developer interested in the exciting world Canonical, Canonical deeply resonates with me, particularly for its unique blend of open-source philosophy, cutting-edge technology, and focus on user experience.
       </p>
-      <h3>What's Appealing</h3>
+      <h3>What&apos;s Appealing</h3>
       <ul>
         <li>
           <strong>Open-Source DNA</strong> Contributing to open-source projects like Vanilla Framework aligns perfectly with my desire to learn, collaborate, and give back to the developer community. Being part of something bigger than myself and directly impacting software used by millions is incredibly motivating.
@@ -41,21 +41,21 @@ const ContextAreaOne = () => {
       <h3>Uncertainties and Inquiries</h3>
       <ul>
         <li>
-          <strong>Balancing Vanilla Framework and Individuality:</strong> While I see the value of central design, I'm curious about opportunities for individual teams to express their brand identity within the framework's structure. Understanding this balance would be helpful.
+          <strong>Balancing Vanilla Framework and Individuality:</strong> While I see the value of central design, I&apos;m curious about opportunities for individual teams to express their brand identity within the framework&apos;s structure. Understanding this balance would be helpful.
         </li>
       </ul>
       <h3>Risky Factors</h3>
       <ul>
         <li>
-          <strong>Rapidly Evolving Tech Landscape:</strong> Keeping up with bleeding-edge technologies can be both exhilarating and challenging. I'm aware of the constant learning curve but am fully committed to continuous improvement.
+          <strong>Rapidly Evolving Tech Landscape:</strong> Keeping up with bleeding-edge technologies can be both exhilarating and challenging. I&apos;m aware of the constant learning curve but am fully committed to continuous improvement.
         </li>
         <li>
-          <strong>Contributing to Large Codebases:</strong> Working on established open-source projects can be a big first dive before you begin to swim and feel comfortable. I'm eager to learn the best customized practices of the Canonical engineering teams and strategies to navigate this aspect.
+          <strong>Contributing to Large Codebases:</strong> Working on established open-source projects can be a big first dive before you begin to swim and feel comfortable. I&apos;m eager to learn the best customized practices of the Canonical engineering teams and strategies to navigate this aspect.
         </li>
       </ul>
       <h3>Overall</h3>
       <p>
-        Despite the uncertainties, Canonical's mission and technology stack ignite my passion for open-source development and accessibility. I'm confident that my eagerness to learn, dedication to quality, and collaborative spirit can be valuable assets to your team. The opportunity to contribute to innovative projects in such a supportive environment makes this role incredibly appealing.
+        Despite the uncertainties, Canonical&apos;s mission and technology stack ignite my passion for open-source development and accessibility. I&apos;m confident that my eagerness to learn, dedication to quality, and collaborative spirit can be valuable assets to your team. The opportunity to contribute to innovative projects in such a supportive environment makes this role incredibly appealing.
       </p>
     </div>
                   </div>
